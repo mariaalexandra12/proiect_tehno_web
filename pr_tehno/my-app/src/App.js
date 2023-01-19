c5269a259ca31e29aec64e396a08b1cc0a1c710f
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-01-20 00:17:58 +0200

    add

--- pr_tehno/my-app/src/App.js
+++ pr_tehno/my-app/src/App.js
@@ -1,12 +1,18 @@
 
 import React from 'react';
 import Login from './View/LogIn/login';
+import {useSelector} from 'react-redux'; //am folosit hookul useSelector pentru a prelua
+import authRedux from './Controllers/Redux/authRedux';
+//info din input 
 
 
 function App() {
+  const {}=useSelector(state=>state)
   return (
    <>
-   <Login />
+   {!authRedux.Login ? 
+    <Login />:
+    <h1>Logat cu succes</h1>}
    </>
     
   );
