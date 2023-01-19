373a8e3ac7a4a1357d65bed94fab1ea0ca341677
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-01-20 00:44:48 +0200

    add

--- pr_tehno/my-app/src/View/Bugs/bug.js
+++ pr_tehno/my-app/src/View/Bugs/bug.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default (props)=>{
+    function Clicked(){
+        props.Clicked(props.name);
+    }
     return (
-        <div className="bug">
+        <div className="bug" onClick={Clicked}>
            <h1 className="name">{props.name}</h1>
            <h2 className="priority">{props.priority}</h2>
            <h3 className="version">{props.version}</h3>
