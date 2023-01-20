c21834f3bc886f070fa60ce55571d7be01ba5b4a
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-01-20 02:19:15 +0200

    add

--- pr_tehno/my-app/src/App.js
+++ pr_tehno/my-app/src/App.js
@@ -2,18 +2,17 @@
 import React from 'react';
 import Login from './View/LogIn/login';
 import {useSelector} from 'react-redux'; //am folosit hookul useSelector pentru a prelua
-import authRedux from './Controllers/Redux/authRedux';
 import PagBug from './View/Bugs/pagBug';
 
 
-const va=[];
+
 function App() {
   const {auth}=useSelector(state=>state)
   return (
    <>
    {!auth.LoggedIn ? //verificam daca este logat 
     <Login />:
-    <PagBug /> //va afisa un mesaj la conectare 
+    <PagBug /> //se va deschide alta pagina cu Bug Details
     }
    </>
     
--- pr_tehno/my-app/src/View/Bugs/pagBug.jsx
+++ pr_tehno/my-app/src/View/Bugs/pagBug.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import '../../View/Bugs/pagBugs.css'
 
 export default ()=> {
-     return ()=>{
+     return (
          <div className="pagBug">
             <form className="pb">
             <h1>Bug Details</h1>
             <button id="view">View Bug</button>
             <button id="create">Create Bug</button>
+            </form>
          </div>
-         </form>
-     }
+         
+     )
 
 }
